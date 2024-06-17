b70e48e5e8b5ba1374428ba1d8d3b283a7db15d2
Jalay Movaliya <76902806+jalaym825@users.noreply.github.com>
2024-06-17 07:04:38 +0530

    Time: 5 ms (34.50%) | Memory: 7 MB (61.35%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.cpp
+++ 633-sum-of-square-numbers/sum-of-square-numbers.cpp
@@ -3,7 +3,9 @@ public:
     bool judgeSquareSum(int c) {
         for(long long int i = 0; i * i <= c; i++)
         {
+            // made b subject from formula
             double b = sqrt(c - i * i);
+            // if b is compelete square, b will be int, return 1
             if(b == (int) b)
                 return 1;
         }
