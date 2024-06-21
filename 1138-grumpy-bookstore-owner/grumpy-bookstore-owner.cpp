4cff2badfd0f7dc7520b1606a922075842279044
Jalay Movaliya <76902806+jalaym825@users.noreply.github.com>
2024-06-21 07:21:06 +0530

    Time: 26 ms (48.29%) | Memory: 34.4 MB (16.52%) - LeetSync

--- 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
@@ -3,7 +3,6 @@ public:
     int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
         int n = customers.size();
         
-        // Step 1: Calculate the initial satisfaction without any change
         int totalSatisfaction = 0;
         for (int i = 0; i < n; ++i) {
             if (grumpy[i] == 0) {
@@ -11,18 +10,14 @@ public:
             }
         }
         
-        // Step 2: Use sliding window to find the maximum extra satisfaction we can gain
         int maxExtraSatisfaction = 0, extraSatisfaction = 0;
         for (int i = 0; i < n; ++i) {
-            // Add the extra satisfaction if grumpy[i] is 1
             if (grumpy[i] == 1) {
                 extraSatisfaction += customers[i];
             }
-            // Remove the extra satisfaction from the start of the window if it exceeds 'minutes'
             if (i >= minutes && grumpy[i - minutes] == 1) {
                 extraSatisfaction -= customers[i - minutes];
             }
-            // Update the maximum extra satisfaction
             maxExtraSatisfaction = max(maxExtraSatisfaction, extraSatisfaction);
         }
         
