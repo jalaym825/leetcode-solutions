b705be83aca0f5ef1f7f1ac071de54eb6709e977
Jalay Movaliya <76902806+jalaym825@users.noreply.github.com>
2024-06-25 23:08:03 +0530

    Time: 2 ms (87.84%) | Memory: 11.3 MB (58.01%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -3,20 +3,22 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int m = triangle.size();
         int n = triangle[m - 1].size();
-        vector<vector<int>> dp(triangle.size(), vector<int>(n, -1));
+        vector<int> prevRow(n);
         for(int i = 0; i < n; i++)
         {
-            dp[m - 1][i] = triangle[m - 1][i];
+            prevRow[i] = triangle[m - 1][i];
         }
         for(int i = n - 2; i >= 0; i--)
         {
+            vector<int> currRow(n);
             for(int j = i; j >= 0; j--)
             {
-                int a = triangle[i][j] + dp[i + 1][j];
-                int b = triangle[i][j] + dp[i + 1][j + 1];
-                dp[i][j] = min(a, b);
+                int a = triangle[i][j] + prevRow[j];
+                int b = triangle[i][j] + prevRow[j + 1];
+                currRow[j] = min(a, b);
             }
+            prevRow = currRow;
         }
-        return dp[0][0];
+        return prevRow[0];
     }
 };
