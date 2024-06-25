14dd8db779cd95a5649b85bd964aef0546964c81
Jalay Movaliya <76902806+jalaym825@users.noreply.github.com>
2024-06-25 21:42:59 +0530

    Time: 7 ms (61.09%) | Memory: 12.5 MB (70.22%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        vector<vector<int>> dp(grid.size(), vector<int>(grid[0].size(), -1));
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<int> prevRow(n, INT_MAX);
+
         for(int i = 0; i < grid.size(); i++)
         {
+            vector<int> currRow(n, INT_MAX);
             for(int j = 0; j < grid[0].size(); j++)
             {
-                if(i == 0 && j == 0)
-                    dp[i][j] = grid[0][0];
-                else
-                    dp[i][j] = min(i - 1 < 0 ? INT_MAX : dp[i - 1][j], j - 1 < 0 ? INT_MAX : dp[i][j - 1]) + grid[i][j];
+                if (i == 0 && j == 0) {
+                    currRow[j] = grid[i][j];
+                } else {
+                    int up = (i > 0) ? prevRow[j] : INT_MAX;
+                    int left = (j > 0) ? currRow[j - 1] : INT_MAX;
+                    currRow[j] = min(up, left) + grid[i][j];
+                }
             }
+            prevRow = currRow;
         }
-        return dp[grid.size() - 1][grid[0].size() - 1];
+        return prevRow[grid[0].size() - 1];
     }
 };
