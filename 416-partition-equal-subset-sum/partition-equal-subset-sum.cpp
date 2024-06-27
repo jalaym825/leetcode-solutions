aa133db70de5e328f6a291ab8d68b42dbd9cc555
Jalay Movaliya <76902806+jalaym825@users.noreply.github.com>
2024-06-27 21:41:36 +0530

    Time: 296 ms (23.56%) | Memory: 16.1 MB (75.69%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
-    bool fun(vector<int>& nums, int i, int sum, int target, vector<vector<int>> &dp) {
-        if (sum == target)
-            return true;
-        if (i >= nums.size() || sum > target)
-            return false;
-        if (dp[i][sum] != -1)
-            return dp[i][sum];
-        
-        if (fun(nums, i + 1, sum + nums[i], target, dp))
-            return dp[i][sum] = true;
-        
-        if (fun(nums, i + 1, sum, target, dp))
-            return dp[i][sum] = true;
-        
-        return dp[i][sum] = false;
-    }
-
     bool canPartition(vector<int>& nums) {
         int totSum = accumulate(nums.begin(), nums.end(), 0);
         if (totSum % 2 != 0)
             return false;
         
         int target = totSum / 2;
-        vector<vector<int>> dp(nums.size(), vector<int>(target + 1, -1));
-        return fun(nums, 0, 0, target, dp);
+        int n = nums.size();
+        
+        vector<vector<bool>> dp(n + 1, vector<bool>(target + 1, false));
+        
+        for (int i = 0; i <= n; i++)
+            dp[i][0] = true;
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= target; j++) {
+                if (nums[i - 1] <= j) {
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        
+        return dp[n][target];
     }
 };
