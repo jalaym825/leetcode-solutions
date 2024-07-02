1060cc78971e42999ed258d1c078dfb802476a7b
Jalay Movaliya <76902806+jalaym825@users.noreply.github.com>
2024-07-02 13:48:53 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (41.05%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -1,64 +1,27 @@
-class Solution
-{
+class Solution {
 public:
-    int search(vector<int> v, int target)
-    {
-        int start = 0;
-        int end = v.size() - 1;
-        int mid = start + ((end - start) / 2);
-        while (start <= end)
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> v1(1001);
+        vector<int> v2(1001);
+        for(int i = 0; i < nums1.size(); i++)
         {
-            if (v[mid] == target)
-            {
-                return mid;
-            }
-            if (target < v[mid])
-            {
-                end = mid - 1;
-            }
-            else
-            {
-                start = mid + 1;
-            }
-            mid = start + ((end - start) / 2);
+            v1[nums1[i]]++;
         }
-        return -1;
-    }
-    vector<int> intersect(vector<int> &nums1, vector<int> &nums2)
-    {
-        vector<int> v;
-        if (nums1.size() > nums2.size())
+        for(int i = 0; i < nums2.size(); i++)
         {
-            sort(nums1.begin(), nums1.end());
-            int i = 0;
-            int isthere;
-            while (i < nums2.size())
+            v2[nums2[i]]++;
+        }
+
+        vector<int> ans;
+        for(int i = 0; i < v1.size(); i++)
+        {
+            if(min(v1[i], v2[i]) > 0)
             {
-                isthere = search(nums1, nums2[i]);
-                if (isthere > -1)
-                {
-                    v.push_back(nums2[i]);
-                    nums1.erase(nums1.begin() + isthere);
-                }
-                i++;
+                for(int j = 0; j < min(v1[i], v2[i]); j++)
+                    ans.push_back(i);
             }
         }
-        else
-        {
-            sort(nums2.begin(), nums2.end());
-            int i = 0;
-            int isthere;
-            while (i < nums1.size())
-            {
-                isthere = search(nums2, nums1[i]);
-                if (isthere > -1)
-                {
-                    v.push_back(nums1[i]);
-                    nums2.erase(nums2.begin() + isthere);
-                }
-                i++;
-            }
-        }
-        return v;
+        return ans;
+
     }
 };
