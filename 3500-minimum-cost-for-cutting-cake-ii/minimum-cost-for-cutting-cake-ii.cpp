50739fb4e1b73abf9148ed694feb3505e7db6696
Jalay Movaliya <76902806+jalaym825@users.noreply.github.com>
2024-07-15 21:57:56 +0530

    Time: 519 ms (11.11%) | Memory: 234.8 MB (11.11%) - LeetSync

---
+++ 3500-minimum-cost-for-cutting-cake-ii/minimum-cost-for-cutting-cake-ii.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    long long minimumCost(int m, int n, vector<int>& hcut, vector<int>& vcut) {
+        sort(hcut.rbegin(), hcut.rend());
+        sort(vcut.rbegin(), vcut.rend());
+
+        int hcuts = 1;
+        int vcuts = 1;
+        long long ans = 0;
+
+        int i = 0, j = 0;
+        while(i < hcut.size() && j < vcut.size())
+        {
+            if(hcut[i] > vcut[j])
+            {
+                ans += hcut[i] * vcuts;
+                hcuts++;
+                i++;
+            }
+            else
+            {
+                ans += vcut[j] * hcuts;
+                vcuts++;
+                j++;
+            }
+        }
+        while(i < hcut.size())
+        {
+            ans += hcut[i] * vcuts;
+            hcuts++;
+            i++;
+        }
+        while(j < vcut.size())
+        {
+            ans += vcut[j] * hcuts;
+            vcuts++;
+            j++;
+        }
+        return ans;
+    }
+};
