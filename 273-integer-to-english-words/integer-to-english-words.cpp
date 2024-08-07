8ee484f8da98cb38d97ed106eda11292557121b9
Jalay Movaliya <76902806+jalaym825@users.noreply.github.com>
2024-08-07 15:36:21 +0530

    Time: 5 ms (32.27%) | Memory: 12.1 MB (50.28%) - LeetSync

---
+++ 273-integer-to-english-words/integer-to-english-words.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<string> lessThan20 = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    vector<string> tens = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    vector<string> thousands = {"", "Thousand", "Million", "Billion"};
+
+    string helper(int num) {
+        if (num == 0) return "";
+        else if (num < 20) return lessThan20[num] + " ";
+        else if (num < 100) return tens[num / 10] + " " + helper(num % 10);
+        else return lessThan20[num / 100] + " Hundred " + helper(num % 100);
+    }
+
+    string numberToWords(int num) {
+        if (num == 0) return "Zero";
+        
+        string result;
+        int i = 0;
+        
+        while (num > 0) {
+            if (num % 1000 != 0) {
+                result = helper(num % 1000) + thousands[i] + " " + result;
+            }
+            num /= 1000;
+            i++;
+        }
+        
+        while (result.back() == ' ') {
+            result.pop_back();
+        }
+        
+        return result;
+    }
+};
