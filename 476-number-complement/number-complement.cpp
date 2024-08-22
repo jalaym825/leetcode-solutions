5f0d9047080e6fc1369ccb8e5eb2a4da9248f769
Jalay Movaliya <76902806+jalaym825@users.noreply.github.com>
2024-08-22 08:50:52 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (8.34%) - LeetSync

--- 476-number-complement/number-complement.cpp
+++ 476-number-complement/number-complement.cpp
@@ -1,20 +1,29 @@
 class Solution {
+private:
+    string intToBin(int num) {
+        string bin;
+        while(num > 0) {
+            bin.push_back(num & 1 ? '1':'0');
+            num >>= 1;
+        }
+        reverse(bin.begin(), bin.end());
+        return bin;
+    }
+    int binToInt(string bin) {
+        int ans = 0;
+        for(int i = 0; i < bin.size(); i++) {
+            if(bin[i] == '1') {
+                ans += 1 << (bin.size() - 1 - i); // Use bitwise shift instead of pow
+            }
+        }
+        return ans;
+    }
 public:
-    int findComplement(int n) {
-    int i;
-    bitset<32> bs(n);
-    string str = bs.to_string();
-    cout<<str<<"\n";
-    for (i = 0; i < str.length(); i++)
-        if (str[i] == '1')
-            break;
-    str.erase(0, i);
-    cout<<str<<"\n";
-    for (i = 0; i < str.length(); i++)
-        str[i] == '1' ? str[i] = '0' : str[i] = '1';
-
-    bitset<32> bits(str);
-    n = bits.to_ullong();
-    return n;
+    int findComplement(int num) {
+        string bin = intToBin(num);
+        for(int i = 0; i < bin.size(); i++) {
+            bin[i] = bin[i] == '0' ? '1' : '0';
+        }
+        return binToInt(bin);
     }
 };
