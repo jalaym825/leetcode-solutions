bb89003ac21a7fdc92c22f63a4d39de48aae357e
Jalay Movaliya <76902806+jalaym825@users.noreply.github.com>
2024-08-29 20:20:00 +0530

    Time: 6 ms (75.98%) | Memory: 24.5 MB (28.57%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        return calculate(nums, target, 0, 0);
+        int maxSum = accumulate(nums.begin(), nums.end(), 0);
+        vector<vector<int>> dp(nums.size(), vector<int>(2 * maxSum + 1, -1));
+        return calculate(nums, target, 0, 0, maxSum, dp);
     }
 
 private:
-    int calculate(vector<int>& nums, int target, int i, int current_sum) {
+    int calculate(vector<int>& nums, int target, int i, int current_sum, int maxSum, vector<vector<int>>& dp) {
         if (i == nums.size()) {
             return current_sum == target ? 1 : 0;
         }
+        if (dp[i][current_sum + maxSum] != -1)
+            return dp[i][current_sum + maxSum];
         
-        int add = calculate(nums, target, i + 1, current_sum + nums[i]);
-        int subtract = calculate(nums, target, i + 1, current_sum - nums[i]);
+        int add = calculate(nums, target, i + 1, current_sum + nums[i], maxSum, dp);
+        int subtract = calculate(nums, target, i + 1, current_sum - nums[i], maxSum, dp);
         
-        return add + subtract;
+        return dp[i][current_sum + maxSum] = add + subtract;
     }
 };
