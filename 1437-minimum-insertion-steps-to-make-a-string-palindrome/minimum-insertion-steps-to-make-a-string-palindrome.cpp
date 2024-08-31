ebe9416359b64bd070a631e35a65a212b8b95ac7
Jalay Movaliya <76902806+jalaym825@users.noreply.github.com>
2024-08-31 12:04:07 +0530

    Time: 32 ms (47.26%) | Memory: 30.1 MB (7.72%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -21,8 +21,22 @@ public:
         string s1 = s;
         string s2 = s1;
         reverse(s2.begin(), s2.end());
-        vector<vector<int>> dp(s1.size(), vector<int>(s2.size(), -1));
+        vector<vector<int>> dp(s1.size() + 1, vector<int>(s2.size() + 1, 0));
+
+        for(int i = 1; i < s1.size() + 1; i++)
+        {
+            for(int j = 1; j < s2.size() + 1; j++)
+            {
+                if(s1[i - 1] == s2[j - 1]) {
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else {
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+
         
-        return s1.size() - longestPalindrome(s1, s2, s1.size()-1, s2.size()-1, dp);
+        return s1.size() - dp[s1.size()][s2.size()];
     }
 };
