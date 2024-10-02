a2910a5738a251f0c48d4a47ba004e1eeb550a20
Jalay Movaliya <76902806+jalaym825@users.noreply.github.com>
2024-10-02 15:32:32 +0200

    Time: 8 ms (57.18%) | Memory: 12.9 MB (65.51%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
-    int fun(int i, int j, string& s1, string& s2, vector<vector<int>>& dp)
-    {
-        if(i == -1)
-            return j + 1;
-        if(j == -1)
-            return i + 1;
-        if(dp[i][j] != -1)
-            return dp[i][j];
+    int minDistance(string s1, string s2) {
+        int m = s1.size(), n = s2.size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
 
-        if(s1[i] == s2[j])
-            return dp[i][j] = fun(i - 1, j - 1, s1, s2, dp);
-        else
-        {
-            // insert at position i
-            int ins = 1 + fun(i, j - 1, s1, s2, dp);
+        // Initialize base cases
+        for (int i = 0; i <= m; i++) dp[i][0] = i;  // Deleting all characters from s1
+        for (int j = 0; j <= n; j++) dp[0][j] = j;  // Inserting all characters to match s2
 
-            // delete
-            int del = 1 + fun(i - 1, j, s1, s2, dp);
-
-            // replace
-            int rep = 1 + fun(i - 1, j - 1, s1, s2, dp);
-
-            return dp[i][j] = min(ins, min(del, rep));
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s1[i - 1] == s2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];  // No operation needed
+                } else {
+                    // Insert, delete, replace
+                    int ins = 1 + dp[i][j - 1];
+                    int del = 1 + dp[i - 1][j];
+                    int rep = 1 + dp[i - 1][j - 1];
+                    dp[i][j] = min(ins, min(del, rep));
+                }
+            }
         }
-    }
-    int minDistance(string word1, string word2) {
-        vector<vector<int>> dp(word1.size(), vector<int>(word2.size(), -1));
-        return fun(word1.size() - 1, word2.size() - 1, word1, word2, dp);
+
+        return dp[m][n];
     }
 };
